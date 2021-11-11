00be07832b847950e9d085af14f3e0d64a81d837
avocardio <mkalcher@uni-osnabrueck.de>
2021-11-10 22:59:44 -0600

    start

--- hw_03/dataset.py
+++ hw_03/dataset.py
@@ -4,3 +4,18 @@ Python script for pre-processing the pipeline
 created on 03.11.2021
 
 """
+import tensorflow as tf
+import tensorflow_datasets as tfds
+
+# Construct a tf.data.Dataset
+ds, ds_info = tfds.load('genomics_ood', shuffle_files=True, as_supervised=True, with_info=True)
+
+print(ds['test'])
+
+# .map(lambda x, y: (tf.cast(tf.reshape(y, (-1,)), y))
+# .map(lambda x, y: ((x/255)-1, y))
+# .map(lambda x, y: (x, tf.one_hot(y, depth=4)))
+# .cache()
+# training.pick(100000)
+# test.pick(1000)
+# .shuffle().batch().prefetch()
